c08417f31df745973e1f43ecf8792ce78eaf64bc
Odison-dev <407572451@qq.com>
2025-11-17 23:11:24 +0800

    把重复的递归构建合并为一个方法，对游戏玩法的更新失败，重新设计关卡

--- Assets/Scripts/Level/LevelCloner.cs
+++ Assets/Scripts/Level/LevelCloner.cs
@@ -19,91 +19,99 @@ public class LevelCloner : MonoBehaviour
     public GameObject RenderCube;
 
     private Constants constants;
-    private GameObject player_s;
-    private GameObject player_b;
-    private GameObject clone_s;
-    private GameObject clone_b;
+    
 
+
+    //委托
+    public delegate void LevelTransform();
+    public static event LevelTransform TransformLevel;
+    private GameObject player_s;
+    private GameObject clone_s;
+    private GameObject player_b;
+    private GameObject clone_b;
     //GameObject player_s = Instantiate(Player);
 
 
     private void Start()
-
     {
         player_s = Instantiate(Player.gameObject);
+        
         player_b = Instantiate(Player.gameObject);
+        
+        
         clone_s = Instantiate(Level.gameObject);
         clone_b = Instantiate(Level.gameObject);
         constants = new Constants();
         RenderCube = GameObject.Find("Renderer");
-        
+        CloneLevel();
+        ClonePlayer();
         PlaceRenderCube();
         
     }
     private void Update()
     {
         
-        CloneLevel();
-        ClonePlayer();
         AdjustPlayer();
     }
 
+
+
+    
+
+
+    //
     private void CloneLevel()
     {
 
         //小号关卡克隆
         //GameObject clone_s = Instantiate (Level.gameObject);
-        clone_s.transform.localScale = Vector3.one * scale;
-        clone_s.transform.position = Level.transform.position + PosOffset;
-        clone_s.transform.eulerAngles = Level.transform.eulerAngles + RotOffset;
+        TransformGameobj("lower", clone_s.transform, Level.transform);
 
         //大号关卡克隆
         //GameObject clone_b = Instantiate(Level.gameObject);
-        clone_b.transform.localScale = Vector3.one / scale;
-        clone_b.transform.position = Level.transform.position - PosOffset / scale;
-        clone_b.transform.eulerAngles = Level.transform.eulerAngles - RotOffset;
+        TransformGameobj("upper", clone_b.transform, Level.transform);
     }
 
     private void ClonePlayer()
     {
-
-        player_s.transform.localScale = Player.transform.localScale * scale;
-        player_s.transform.position = (Player.transform.position - Level.transform.position) * scale + Level.transform.position + PosOffset;
-        player_s.transform.eulerAngles = Player.transform.eulerAngles + RotOffset;
+        //GameObject player_s = Instantiate(Player.gameObject);
+        TransformGameobj("lower", player_s.transform, Player.transform);
 
 
-
-        player_b.transform.localScale = Player.transform.localScale / scale;
-        player_b.transform.position = (Player.transform.position - Level.transform.position) / scale + Level.transform.position - PosOffset / scale;
-        player_b.transform.eulerAngles = Player.transform.eulerAngles + RotOffset;
+        //GameObject player_b = Instantiate(Player.gameObject);
+        TransformGameobj("upper", player_b.transform, Player.transform);
     }
     
     private void PlaceRenderCube()
     {
-       
-
-        RenderCube.transform.localScale = new Vector3(1, 1, 1) * scale * sidelength;
-        RenderCube.transform.position = Level.transform.position + PosOffset - Vector3.forward * 5;
-        //RenderCube.transform.position = Level.transform.position + PosOffset + new Vector3(PosOffset2.x * Mathf.Sin(RotOffset.z * 2), PosOffset2.y * Mathf.Cos(RotOffset.z * 2), RenderCube.transform.position.z);
-        RenderCube.transform.eulerAngles = RotOffset * 2 + Vector3.forward * 180 + Level.transform.eulerAngles + RotOffset;
-
         
+        RenderCube.transform.localScale = new Vector3(1, 1, 1) * scale * scale * sidelength;
+        RenderCube.transform.position = Level.transform.position + PosOffset - Vector3.forward * 5 + Quaternion.Euler(0, 0, RotOffset.z) * PosOffset * scale;
         
+        RenderCube.transform.eulerAngles = RotOffset * 2 + Vector3.forward * 180 + Level.transform.eulerAngles;
     }
 
     private void AdjustPlayer()
     {
-        if (IsCollidedSmall())
+        TransformGameobj("lower", player_s.transform, Player.transform);
+        TransformGameobj("upper", player_b.transform, Player.transform);
+        if (IsInSmall())
         {
-            
-            Player.transform.position = player_b.transform.position;
-            Player.transform.eulerAngles = player_b.transform.eulerAngles;
-            Player.transform.localScale = player_b.transform.localScale;
+
+            TransformGameobj("upper", Player.transform, Player.transform);
             Player.GetComponent<Rigidbody2D>().velocity /= scale;
+            Player.GetComponent<Rigidbody2D>().gravityScale /= scale;
+            //Player.transform = player_b.transform; 
+        }
+        if (IsInBig())
+        {
+            TransformGameobj("lower", Player.transform, Player.transform);
+            Player.GetComponent<Rigidbody2D>().velocity *= scale;
+            Player.GetComponent<Rigidbody2D>().gravityScale *= scale;
         }
     }
 
-    private bool IsCollidedSmall()
+    private bool IsInSmall()
     {
         Vector2 distance = Player.transform.position - clone_s.transform.position;
         Vector2 new_vector = new Vector2(distance.x * Mathf.Cos(RotOffset.z) - distance.y * Mathf.Sin(RotOffset.z), distance.x * Mathf.Sin(RotOffset.z) + distance.y * Mathf.Cos(RotOffset.z));
@@ -111,6 +119,45 @@ public class LevelCloner : MonoBehaviour
         {
             return true;
         }
-            return false;
+
+        return false;
     }
+
+    private bool IsInBig()
+    {
+        Vector2 distance = Player.transform.position - Level.transform.position;
+
+        if (Mathf.Abs(Mathf.Acos(Vector2.Dot(distance, Level.transform.up) / (distance.magnitude * Level.transform.up.magnitude))) < 45) 
+        {
+            if (Mathf.Abs(Vector2.Dot(distance, Level.transform.up))> sidelength / 2) { return true; }
+            
+
+        }
+        if (Mathf.Abs(Mathf.Acos(Vector2.Dot(distance, Level.transform.right) / (distance.magnitude * Level.transform.right.magnitude))) < 45)
+        {
+            if (Mathf.Abs(Vector2.Dot(distance, Level.transform.right)) > sidelength / 2) { return true; }
+
+
+        }
+        return false;
+    }
+
+    private void TransformGameobj(string type, Transform objTransform,Transform originObj)
+    {
+        if (type == "lower")
+        {
+            objTransform.transform.position = Quaternion.Euler(0, 0, RotOffset.z) * (originObj.transform.position - Level.transform.position) * scale + PosOffset + Level.transform.position;
+            objTransform.transform.localScale = originObj.transform.localScale * scale;
+            objTransform.transform.eulerAngles = originObj.transform.eulerAngles + RotOffset;
+            
+        }
+        else if (type == "upper")
+        {
+            objTransform.transform.position = Quaternion.Euler(0, 0, -RotOffset.z) * (originObj.transform.position - Level.transform.position) / scale + Level.transform.position - Quaternion.Euler(0, 0, -RotOffset.z) * (PosOffset / scale);
+            objTransform.transform.localScale = originObj.transform.localScale / scale;
+            objTransform.transform.eulerAngles = originObj.transform.eulerAngles - RotOffset;
+        }
+        //return objTransform;
+    }
+    
 }
